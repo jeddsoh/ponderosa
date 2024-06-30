2dd4f514ce838b16a8277ab95c1ff82442113144
Jedd Soh <jeddsoh@gmail.com>
2024-06-30 02:26:24 -0500

    feat: integrate NextUI with project setup and configure TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import {Button} from '@nextui-org/button'; 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, World</h1>
+      <Button>Test</Button>
     </main>
   )
 }
